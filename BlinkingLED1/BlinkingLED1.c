829a0abb5959f548ba63b785bcd694bdabba6741
Thomas Fischer <tfischer3@online.gibz.ch>
2020-05-05 11:29:47 +0200

    Blinking LED

---
+++ BlinkingLED1/BlinkingLED1.c
@@ -0,0 +1,38 @@
+#include <avr\io.h>
+#include <Atmega328P.h>
+#include <avrlib.h>
+#include <RegisterAccess.h>
+#define F_CPU 16000000
+#include <util\delay.h>
+#include <inttypes.h>
+#include <avr/interrupt.h>
+
+
+
+void ConfigPins(Bool status) {
+
+    SetRegister(PortB.DDR, (PIN_3, DdrOutput), (PIN_4, DdrOutput), (PIN_5, DdrInput));
+    SetRegister(PortB.PORT, (PIN_3, status), (PIN_4, 0));
+
+}
+
+int main(void)
+{
+    Usart_Init(250000); // higher is to fast; cannot be consumed reliably anymore!
+
+    Bool status = False;
+
+    ConfigPins(status);
+
+    while (True)
+    {
+        TRACE("Hello world asdasdas1");
+
+        status = !status;
+
+        UpdateRegister(PortB.PORT, (PIN_3, status));
+
+        _delay_ms(1000);
+    }
+    return 0;
+}
---
+++ BlinkingLED1/Build/HelloWorld.c
@@ -0,0 +1,29 @@
+#include <avr\io.h>
+#include <Atmega328P.h>
+#include <avrlib.h>
+#include <RegisterAccess.h>
+#define F_CPU 16000000
+#include <util\delay.h>
+#include <inttypes.h>
+#include <avr/interrupt.h>
+
+
+int main(void)
+{
+    Usart_Init(250000); // higher is to fast; cannot be consumed reliably anymore!
+
+    Usart_Trace0(1);
+
+
+
+    //LedConfigurePin();
+    //Bool status = True;
+    while (True)
+    {
+        Usart_Trace0(2);
+        /*UpdateRegister(PortB.PORT, (PIN_3, status));
+        status = !status;*/
+        _delay_ms(1000);
+    }
+    return 0;
+}
